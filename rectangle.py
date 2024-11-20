c0a6ac71a41e7bc4ef981f78fb38c6facaa815fd
Tanu200410 <120491945+Tanu200410@users.noreply.github.com>
2024-11-20 10:43:41 +0000

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        if self.width < 0 or self.height < 0:
+            return -1
+        else:
+            return self.width * self.height
+
+    def get_perimeter(self):
+        if self.width < 0 or self.height < 0:
+            return -1
+        else:
+            return 2 * (self.width + self.height)
